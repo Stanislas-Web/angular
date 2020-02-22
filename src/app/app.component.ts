6ff21ca6ee10393ab53daf2380ac1f1c98ad9044
Your Name <you@example.com>
2020-02-22 14:25:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+  appareils=[
+    {
+      name:'Machine à laver',
+      status:'éteint'
+    },
+    {
+      name:'Frigo',
+      status:'allumé'
+    },
+    {
+      name:'Ordinateur',
+      status:'éteint'
+    }
+  ];
+  constructor(){
+    setTimeout(()=>{
+      this.isAuth= true;
+    },4000)
+  }
+  onAllumer(appareils){
+    this.appareils=[
+      {
+        name:'Machine à laver',
+        status:'allumé'
+      },
+      {
+        name:'Frigo',
+        status:'allumé'
+      },
+      {
+        name:'Ordinateur',
+        status:'allumé'
+      }
+    ];
+
+  }
+  onEteint(appareils){
+    this.appareils=[
+      {
+        name:'Machine à laver',
+        status:'éteint'
+      },
+      {
+        name:'Frigo',
+        status:'éteint'
+      },
+      {
+        name:'Ordinateur',
+        status:'éteint'
+      }
+    ];
+  }
+  onAll(appareils){
+    this.appareils=[
+      {
+        name:'Machine à laver',
+        status:'allumé'
+      },
+      {
+        name:'Frigo',
+        status:'allumé'
+      },
+      {
+        name:'Ordinateur',
+        status:'allumé'
+      }
+    ];
+
+  }
 }
